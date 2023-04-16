2ef8bb0e66edb82753800541fba48ef897eb9d5e
David-RF35 <121755571+David-RF35@users.noreply.github.com>
2023-04-15 23:30:12 -0300

    Update BorboletaCalc.java

--- BorboletaCalc.java
+++ BorboletaCalc.java
@@ -1,5 +1,3 @@
-package javatpoint.exemplos;
-
 import java.util.Scanner;
 
 public class BorboletaCalc {
@@ -25,17 +23,17 @@ public class BorboletaCalc {
 			System.out.println("digite o valor da asa3");
 			double asa3_p=scan.nextDouble();
 			
-			double bb_valor=asa1_p+asa3_p-(asa2_m*2);
+			double bb_valor=asa1_p+asa3_p-(asa2_m*2);//processamento do preço da estratégia
 			
 			System.out.println("O valor da borboleta é: "+ bb_valor+ " centavos ");
 			
-		if(bb_valor<=0.14) {
+		if (bb_valor<=0.14) {
 			
 			System.out.println("Borboleta barata, entrada ok");
 			
 		}
 		
-			else if(bb_valor>0.14 && bb_valor <=0.17) {
+			else if (bb_valor>0.14 && bb_valor <=0.17) {
 			
 			System.out.println("Borboleta entrada atrasada, menor potencial de ganho.");
 		}	
